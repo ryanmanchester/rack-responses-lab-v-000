10c44433db26b417a4d1cb6d668b5794e110f497
Ryan Manchester <ryanm17@gmail.com>
2018-05-12 19:42:51 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,4 +1,3 @@
-require 'pry'
 class Application
 
   def call(env)
@@ -6,7 +5,7 @@ class Application
 
     time = Time.now
 
-    if time.hour < 12 
+    if time.hour < 12
       resp.write "Good Morning!"
     else
       resp.write "Good Afternoon"
